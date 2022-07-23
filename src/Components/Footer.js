045f2befe98e856ae6703a577bed7617210c3230
Indrajit Pal <palindrajit10@gmail.com>
2022-07-23 13:10:47 +0530

    Added alert and footer components

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+function Footer({ darkMode }){
+
+    const myStyle = {
+        backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    }
+
+    return(
+        
+        <>
+        <footer className={`text-center text-lg-start bg-${darkMode} text-muted`}>
+        <div className="text-center p-4" style = {myStyle} >
+            © 2021 Copyright:
+            <a className="text-reset fw-bold" href="https://www.linkedin.com/in/indrajit-pal-a17975218/">iNDRAjiT</a>
+        </div>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
