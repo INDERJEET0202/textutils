3f463953c51831ebf4bde5a67ef9da1824dbb776
Indrajit Pal <palindrajit10@gmail.com>
2022-07-23 13:11:27 +0530

    showAlert function added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import About from './Components/About';
+import Footer from './Components/Footer';
 import { useState } from 'react';
+import Alert from './Components/Alert';
 
 function App() {
   const [darkMode, setDarkMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 15000)
+  }
 
   const toggleMode = () => {
     if(darkMode === 'dark') {
       setDarkMode('light');
       document.body.style.backgroundImage = 'linear-gradient(to bottom right, #FFFFFF, #DFDFDF)';
       document.body.style.color = 'black';
+      showAlert('Light Mode enabled', 'success');
     }
     else {
       setDarkMode('dark');
       document.body.style.backgroundImage = 'linear-gradient(to bottom right, grey, #122B40)';
       document.body.style.color = 'white';
+      showAlert('Dark Mode enabled', 'success');
     }
   }
 
@@ -24,9 +39,11 @@ function App() {
     <>
     <Navbar title = {"TextUtils"} about = {"About TextUtils"} darkMode = {darkMode} toggleMode = {toggleMode}/>
     <div className="container my-3">
-      <TextForm heading = {"Enter the text to analyze below!"}/>
+      <Alert alert = {alert}/>
+      <TextForm heading = {"Enter the text to analyze below!"} showAlert = {showAlert}/>
       <About/>
     </div>
+    <Footer darkMode = {darkMode}/>
     </>
   );
 }
